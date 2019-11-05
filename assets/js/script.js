5e8d1b63f8bf8683811e65fc61515c6d47a8aa3d
jenny80170 <jennifer.malleret@outlook.fr>
2019-11-05 15:45:18 +0100

    Au click sur le bouton afficher une boite à dialogue

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// Appel de la librairie Jquery
+$(function(){
+  // Fonction pour afficher une boite à dialogue lors du click
+  $('#clickMe').click(function(){
+    // Alerte pour afficher la boite à dialogue
+    alert('Vous avez cliquez sur le bouton');
+  });
+});
